aec9d20c01b6d532afb571bbd292cd6d58e6c89d
stefanuspn <stefanusnugroho585@gmail.com>
2018-01-22 21:03:28 +0700

    Create 404.php

---
+++ app/view/error/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['path'] ;?>css/style.css">
+</head>
+<body>
+	<div id="container">
+	<h1>404 NOT FOUND</h1>
+
+	<div id="body">
+		<p>Not Found Controller</p>
+	</div>
+
+	<p class="footer">CopyRight By Stefanus Prasetyo Nugroho</p>
+
+	
+</body>
+</html>
+
+
+ 
